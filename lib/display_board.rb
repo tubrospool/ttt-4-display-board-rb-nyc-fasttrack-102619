ecec689a6eba5a654e89db7ff4d114ed9c0620c3
Ken Ho <kenmho38@hotmail.com>
2019-12-14 22:36:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -27,3 +27,18 @@ end
 
 
 display_board(board)
+
+
+board = [" "," ","O"," ",""," "," "," "," "]
+
+
+def display_board(board)
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts " ---------- "
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts " ---------- "
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+display_board(board)
